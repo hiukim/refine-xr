6a678e1125a58d04589331120397e90b6a708419
hiukim <hiukim528@gmail.com>
2023-02-23 18:08:40 -0800

    update start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -3,4 +3,6 @@
 ( \
 (cd fake-rest-server && npm run dev) \
 & (cd next-app && npm run dev) \
+& (local-ssl-proxy --source 3001 --target 3000) \
+& (local-ssl-proxy --source 4001 --target 4000) \
 )

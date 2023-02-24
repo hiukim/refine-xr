f87d2434ab528481fa4cbe6ec3b0ec3745bad555
hiukim <hiukim528@gmail.com>
2023-02-23 18:07:56 -0800

    fix loaders

--- next-app/libs/loaders.js
+++ next-app/libs/loaders.js
@@ -1,8 +1,15 @@
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+//import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 export const loadGLTF = (path) => {
   return new Promise((resolve, reject) => {
     const loader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderConfig({ type: 'js' });
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+    
+    loader.setDRACOLoader( dracoLoader);
     loader.load(path, (gltf) => {
       resolve(gltf);
     });
